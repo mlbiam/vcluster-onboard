3ae7254cf7e436acb42d5235eb5ed643031f1297
mlbiam <marc@tremolo.io>
2022-10-12 14:17:56 -0400

    fix download

--- install-vcluster.sh
+++ install-vcluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && chmod +x vcluster; && chmod +x vcluster;
+curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" ; chmod +x vcluster; chmod +x vcluster;
 mv vcluster /usr/local/bin;
