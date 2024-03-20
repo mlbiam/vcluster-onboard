b7315a7746a6bfa945f9621b5648062977e51c26
Marc Boorshtein <marc@tremolo.io>
2024-03-20 11:28:56 +0100

    removing manual ca cert creation, now integrated into the chart

--- onboard-vcluster-to-controlplane.sh
+++ onboard-vcluster-to-controlplane.sh
@@ -23,16 +23,6 @@ kubectl ctx
 kubectl create ns kubernetes-dashboard
 helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard  --version 6.0.8  --set settings.clusterName=$VCLUSTER_LABEL --set settings.itemsPerPage=15 -n kubernetes-dashboard
 
-
-if [ -v CA_CRT ]; then
-    mkdir -p /tmp/cert-secret
-    echo -n $CA_CRT | base64 -d > /tmp/cert-secret/tls.crt
-    kubectl create ns openunison
-    kubectl create secret generic unison-ca --from-file=/tmp/cert-secret -n openunison
-else
-    echo "No CA Cert"
-fi
-
 kubectl ctx controlplane
 
 kubectl ouctl install-satelite -r cluster-role-bindings=tremolo/openunison-vcluster-admins /etc/openunison/satelite.yaml controlplane $VCLUSTER_CTX
