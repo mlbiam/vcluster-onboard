fcf51c93fac39734d965f3112b51c1fe47b1362a
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-08-16 17:35:46 -0400

    make onbaord script configurable

--- onboard-cluster.sh
+++ onboard-cluster.sh
@@ -2,10 +2,15 @@
 
 export PATH=$PATH:~/.krew/bin
 
-
+if [ -v ONBOARD_SCRIPT ];then
+    echo "Running onboard script $ONBOARD_SCRIPT"
+else
+    export ONBOARD_SCRIPT="/usr/local/openunison/onboard-vcluster-to-controlplane.sh"
+    echo "Running onboard script $ONBOARD_SCRIPT"
+fi
 
 kubectl config set-cluster controlplane --server=https://kubernetes.default.svc:443 --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 kubectl config set-credentials controlplane --token=/var/run/secrets/kubernetes.io/serviceaccount/token
 kubectl config set-context controlplane --user=controlplane --user=controlplane
 
-vcluster connect $VCLUSTER_NAME  -n $VCLUSTER_NAMESPACE -- /usr/local/openunison/onboard-vcluster-to-controlplane.sh
+vcluster connect $VCLUSTER_NAME  -n $VCLUSTER_NAMESPACE -- $ONBOARD_SCRIPT
